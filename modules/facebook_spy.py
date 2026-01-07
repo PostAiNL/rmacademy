0eb3c69663f60d254fcde23b0597d7b275dbc9b6
PostAiNL <davitsio@gmail.com>
2026-01-07 16:04:07 +0100

    Add files via upload

--- modules/facebook_spy.py
+++ modules/facebook_spy.py
@@ -2,44 +2,44 @@ import streamlit as st
 from apify_client import ApifyClient
 import urllib.parse
 
-def search_facebook_ads(query, max_results=20):
-    """
-    Gebruikt de officiele apify/facebook-ads-scraper.
-    Kosten: ca. $0.20 per succesvolle scan.
-    """
+def search_facebook_ads(query, max_results=15): # We verlagen het naar 15 voor meer succes
     client = ApifyClient(st.secrets["apify"]["token"])
 
-    # We bouwen de zoek-URL die de robot nodig heeft
-    # We zoeken standaard in Nederland ('NL') en op alle actieve ads
     encoded_query = urllib.parse.quote(query)
-    start_url = f"https://www.facebook.com/ads/library/?active_status=all&ad_type=all&q={encoded_query}&country=NL&media_type=all"
+    # We voegen de taal toe aan de URL
+    start_url = f"https://www.facebook.com/ads/library/?active_status=active&ad_type=all&q={encoded_query}&country=NL&media_type=all"
 
     run_input = {
         "startUrls": [{"url": start_url}],
-        "maxAds": max_results, # We zetten hem op 20 om kosten te besparen
+        "maxAds": max_results,
         "activeStatus": "active",
         "viewAllAds": True,
+        # NIEUW: We dwingen de robot om heel traag en menselijk te werken
+        "maxConcurrency": 1, 
         "proxyConfiguration": {
             "useApifyProxy": True,
-            "apifyProxyGroups": ["RESIDENTIAL"] # CRUCIAAL voor Facebook
+            "apifyProxyGroups": ["RESIDENTIAL"],
+            "apifyProxyCountry": "NL" # We dwingen een Nederlandse proxy af
         }
     }
 
     try:
-        # Start de robot
-        run = client.actor("apify/facebook-ads-scraper").call(run_input=run_input)
+        # We gebruiken een langere timeout zodat de robot de tijd krijgt
+        run = client.actor("apify/facebook-ads-scraper").call(run_input=run_input, timeout_secs=120)
 
-        # Haal de resultaten op
         results = []
         for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-            # We pakken alleen de info die we echt nodig hebben
             results.append({
                 "page_name": item.get("pageName", "Onbekend"),
-                "media": item.get("adSnapshotUrl"), # Screenshot van de ad
-                "days_active": 1, # De officiele scraper geeft vaak geen exacte begindatum, we zetten hem op 1
-                "shop_link": item.get("ctaLink", "#") # De link naar de webshop
+                "media": item.get("adSnapshotUrl"),
+                "days_active": 1,
+                "shop_link": item.get("ctaLink", "#")
             })
         
+        # Als we ondanks alles 0 resultaten hebben, is hij waarschijnlijk alsnog geblokkeerd
+        if len(results) == 0:
+            return "ERROR"
+            
         return results
 
     except Exception as e:
